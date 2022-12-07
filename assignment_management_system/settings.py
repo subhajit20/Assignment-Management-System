4b04f0068b80d2e6240a6a7d71a83727e6b25ee7
Subhajit Ghosh <subhajitstd07@gmail.com>
2022-12-07 12:35:01 +0530

    made build_files.sh and verecl.json

--- assignment_management_system/settings.py
+++ assignment_management_system/settings.py
@@ -260,3 +260,15 @@ CELERY_TIMEZONE = "Asia/Kolkata"
 #         'LOCATION': 'my_cache_table',
 #     }
 # }
+
+import os
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIR = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
